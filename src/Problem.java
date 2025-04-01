1971b90c6639e3ab8e510561c463cc371c9876e7
Diana <Khachaturova.Diana@student.greenriver.edu>
2025-04-01 14:40:54 -0700

    Started the pair assignment, added a private helper

--- src/Problem.java
+++ src/Problem.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Problem {
     /**
      * Returns how many salamanders less than 3 years old will be at risk from a
@@ -8,13 +10,20 @@ public class Problem {
      * and so on. Salamanders of any age can catch and spread the disease, but
      * it is only a serious risk for salamanders strictly less than 3 years old.
      * 
-     * Include the initial patient in the count iff the initial patient is less
+     * Include the initial patient in the count if the initial patient is less
      * than 3 years old.
      * 
      * @param initialPatient the first salamander to be infected
      * @return the number of salamanders less than 3 years old that may be infected
      */
     public static int countSeriousInfections(Salamander initialPatient) {
+        if (initialPatient == null) return 0;
+
+        Set<Salamander> visited = new HashSet<>();
+        return countSeriousInfectionsHelper(initialPatient, visited);
+    }
+
+    private static int countSeriousInfectionsHelper(Salamander current, Set<Salamander> visited) {
         return -1;
     }
 }
