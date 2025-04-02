48b2a7c515242278f37a240aecb5e59a45e5262c
Ben Mojo <155706745+oakes777@users.noreply.github.com>
2025-04-01 21:32:38 -0700

    JS commit again as last commit didn't work

--- src/Problem.java
+++ src/Problem.java
@@ -17,27 +17,52 @@ public class Problem {
      * @return the number of salamanders less than 3 years old that may be infected
      */
     public static int countSeriousInfections(Salamander initialPatient) {
-        Set<Salamander> visited = new HashSet<>(); 
-        Queue<Salamander> cueBall = new LinkedList<>();
+        //initialize HashSet to track visited nodes
+        Set<Salamander> visited = new HashSet<>();
+        //return helper/recursion method
+        return dfs(initialPatient, visited);
 
-        int count = 0;
-        cueBall.add(initialPatient);
+        //I wanted to accomplish without helper method
+        //This works but uses BFS instead of DFS 
+        //So I commented this out but wanted to keep it
+        
+        // Set<Salamander> visited = new HashSet<>(); 
+        // Queue<Salamander> cueBall = new LinkedList<>();
 
-        while (!cueBall.isEmpty()) {
-            Salamander current = cueBall.poll();
+        // int count = 0;
+        // cueBall.add(initialPatient);
 
-            if (visited.contains(current)) continue;
-            visited.add(current);
+        // while (!cueBall.isEmpty()) {
+        //     Salamander current = cueBall.poll();
 
-            if (current.getAge() < 3) {
-                count++;
-            }
+        //     if (visited.contains(current)) continue;
+        //     visited.add(current);
 
-            for (Salamander contact : current.getContacts()) {
-                if (!visited.contains(contact)) {
-                    cueBall.add(contact);
-                }
-            }
+        //     if (current.getAge() < 3) {
+        //         count++;
+        //     }
+
+        //     for (Salamander contact : current.getContacts()) {
+        //         if (!visited.contains(contact)) {
+        //             cueBall.add(contact);
+        //         }
+        //     }
+        // }
+        // return count;
+    }
+
+    private static int dfs(Salamander slay, Set<Salamander> visited) {
+        //base case
+        if (visited.contains(slay)) return 0;
+        visited.add(slay);
+
+        //ternary op - condition (slay.getAge() < 3)
+        int count = slay.getAge() < 3 ? 1 : 0;
+
+        //for each iteration
+        for (Salamander contact : slay.getContacts()) {
+            //add recursion result to count
+            count += dfs(contact, visited);
         }
         return count;
     }
