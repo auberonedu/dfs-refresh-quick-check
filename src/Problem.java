f735132a4ce19f043ba704c8e2f23783fa8e64af
Stefanurkal <stephenfranada@gmail.com>
2025-04-02 20:52:56 -0700

    completed

--- src/Problem.java
+++ src/Problem.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Problem {
     /**
      * Returns how many salamanders less than 3 years old will be at risk from a
@@ -8,13 +11,30 @@ public class Problem {
      * and so on. Salamanders of any age can catch and spread the disease, but
      * it is only a serious risk for salamanders strictly less than 3 years old.
      * 
-     * Include the initial patient in the count iff the initial patient is less
+     * Include the initial patient in the count if the initial patient is less
      * than 3 years old.
      * 
      * @param initialPatient the first salamander to be infected
      * @return the number of salamanders less than 3 years old that may be infected
      */
     public static int countSeriousInfections(Salamander initialPatient) {
-        return -1;
+        Set<Salamander> visited = new HashSet<>();
+        
+        return countSeriousInfectionsDFS(initialPatient, visited);
+
+    }
+    private static int countSeriousInfectionsDFS(Salamander current, Set<Salamander> visited) {
+        if (visited.contains(current)) {
+            return 0;
+        }
+
+        visited.add(current);
+        
+        int count = (current.getAge() < 3) ? 1 : 0;
+        
+        for (Salamander neighborSalamander : current.getContacts()) {
+            count += countSeriousInfectionsDFS(neighborSalamander, visited);
+        }
+        return count; 
     }
 }
